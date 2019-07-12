0cc594f10a412a871671fc4e99e8286e1b0dd1b4
kwondajin <kdjj214@nate.com>
2019-07-12 14:40:05 +0900

    bitcamp-java-application 16 버전

--- bitcamp-java-application/src/main/java/com/eomcs/lms/handler/LessonList.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/handler/LessonList.java
@@ -3,16 +3,31 @@ package com.eomcs.lms.handler;
 import com.eomcs.lms.domain.Lesson;
 
 public class LessonList {
+  private static final int DEFAULT_CAPACITY = 100;
+
   private Lesson[] list = new Lesson[100];
   private int size = 0;
-  
-  public void add(Lesson lesson) {
-    this.list[this.size++] = lesson; 
+
+  public LessonList() {
+    this(DEFAULT_CAPACITY);
   }
-  
+
+  public LessonList(int initialCapacity) {
+    if (initialCapacity < 50 || initialCapacity > 10000)
+      list = new Lesson[DEFAULT_CAPACITY];
+    else
+      list = new Lesson[initialCapacity];
+  }
+
+  public void add(Lesson lesson) {
+    if(this.size == list.length)
+      throw new RuntimeException("배열이 꽉 찼습니다");
+    this.list[this.size++] = lesson;
+  }
+
   public Lesson[] toArray() {
     Lesson[] arr = new Lesson[this.size];
-    for(int i = 0; i < this.size; i++) {
+    for (int i = 0; i < this.size; i++) {
       arr[i] = this.list[i];
     }
     return arr;
--- bitcamp-java-application/src/main/java/com/eomcs/lms/handler/MemberList.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/handler/MemberList.java
@@ -3,16 +3,31 @@ package com.eomcs.lms.handler;
 import com.eomcs.lms.domain.Member;
 
 public class MemberList {
+  private static final int DEFAULT_CAPACITY = 100;
+
   private Member[] list = new Member[100];
   private int size = 0;
-  
+
+  public MemberList() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  public MemberList(int initialCapacity) {
+    if (initialCapacity < 50 || initialCapacity > 10000)
+      list = new Member[DEFAULT_CAPACITY];
+    else
+      list = new Member[initialCapacity];
+  }
+
   public void add(Member member) {
+    if(this.size == list.length)
+      throw new RuntimeException("배열이 꽉 찼습니다");
     this.list[size++] = member;
   }
-  
+
   public Member[] toArray() {
     Member[] arr = new Member[size];
-    for(int i = 0; i < this.size; i++) {
+    for (int i = 0; i < this.size; i++) {
       arr[i] = this.list[i];
     }
     return arr;
