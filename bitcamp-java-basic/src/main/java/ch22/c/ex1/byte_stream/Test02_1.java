fb3869de3c29c111b8d5099ba3c56b3cbc012939
kwondajin <kdjj214@nate.com>
2019-07-29 14:43:37 +0900

    파일 입출력 예제

--- bitcamp-java-basic/src/main/java/ch22/c/ex1/byte_stream/Test02_1.java
+++ bitcamp-java-basic/src/main/java/ch22/c/ex1/byte_stream/Test02_1.java
@@ -4,32 +4,25 @@ package ch22.c.ex1.byte_stream;
 import java.io.FileOutputStream;
 
 public class Test02_1 {
-  public static void main(String[] args) {
-    
-    FileOutputStream out = null;
-    
-    try {
-      out = new FileOutputStream("temp/data.bin");
-      
-      System.out.println("데이터 쓰는 중...");
-      
-      long start = System.currentTimeMillis();
-      
-      for (int i = 0; i < 1000000; i++) {
-        out.write(0x55);
-      }
-      
-      long end = System.currentTimeMillis();
-      
-      System.out.println(end - start);
-      
-    } catch (Exception e) {
-      e.printStackTrace();
-      
-    } finally {
-      try {out.close();} catch (Exception e) {}
+  public static void main(String[] args) throws Exception {
+
+
+    FileOutputStream out = new FileOutputStream("temp/data.bin");
+
+    System.out.println("데이터 쓰는 중...");
+
+    long start = System.currentTimeMillis();
+
+    for (int i = 0; i < 1000000; i++) {
+      out.write(0x55);
     }
-    
+
+    long end = System.currentTimeMillis();
+
+    System.out.println(end - start);
+
+    out.close();
+
     System.out.println("출력 완료!");
   }
 }
