75d86bc7bba23c820fc3d4f0f2bf6daee0cb5614
kwondajin <kdjj214@nate.com>
2019-08-13 15:33:39 +0900

    37_5

---
+++ bitcamp-java-application4-server/v37_5/src/main/java/com/eomcs/lms/handler/MemberSearchCommand.java
@@ -0,0 +1,35 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.List;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.Input;
+
+public class MemberSearchCommand implements Command {
+
+  private MemberDao memberDao;
+
+
+  public MemberSearchCommand(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+    try {
+      String keyword = Input.getStringValue(in, out, "검색어? ");
+      
+      List<Member> members = memberDao.findbyKeyword(keyword);
+      for (Member member : members) {
+        out.printf("%s, %s, %s, %s, %s\n", member.getNo(), member.getName(), member.getEmail(),
+            member.getTel(), member.getResisteredDate());
+      }
+    } catch (Exception e) {
+      out.println("데이터 목록 조회 실패!!!");
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
