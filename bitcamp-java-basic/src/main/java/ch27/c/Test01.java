add9c429e60fa9391bf2e24019893f363c3fcf38
unknown <kdjj214@nate.com>
2019-08-30 15:43:38 +0900

    Bean Container 적용하기

--- bitcamp-java-basic/src/main/java/ch27/c/Test01.java
+++ bitcamp-java-basic/src/main/java/ch27/c/Test01.java
@@ -14,12 +14,20 @@ public class Test01 {
     // => 기본 생성자가 있을 때만 가능하다.
     // => 향후 삭제될 메서드이다. 가능한 사용하지 말라!
     Student s1 = (Student) clazz.newInstance();
+    s1.setName("홍길동");
+    s1.setEmail("hong@test.com");
+    s1.setWorking(true);
+    s1.setAge(20);
+    System.out.println(s1);
+    System.out.println("--------------------------");
     
     // 생성자를 통해 인스턴스를 생성하라!
     // => 기본 생성자 꺼내기
     @SuppressWarnings("unchecked")
     Constructor<Student> c1 = (Constructor<Student>) clazz.getConstructor();
     Student s2 = c1.newInstance();
+    System.out.println(s2 != null);
+    System.out.println("--------------------------");
     
     // => 파라미터를 받는 생성자 꺼내기
     @SuppressWarnings("unchecked")
