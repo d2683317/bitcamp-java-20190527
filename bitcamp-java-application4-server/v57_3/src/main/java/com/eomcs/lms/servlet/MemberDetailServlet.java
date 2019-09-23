0c15f264cbe9660130aa74e2b0e82a54c18b866a
unknown <kdjj214@nate.com>
2019-09-23 11:38:19 +0900

    JSP에 JSTL 적용하기

---
+++ bitcamp-java-application4-server/v57_3/src/main/java/com/eomcs/lms/servlet/MemberDetailServlet.java
@@ -0,0 +1,46 @@
+package com.eomcs.lms.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+
+@WebServlet("/member/detail")
+public class MemberDetailServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  private MemberDao memberDao;
+  
+  @Override
+  public void init() throws ServletException {
+    ApplicationContext appCtx = (ApplicationContext) getServletContext().getAttribute("iocContainer");
+    memberDao = appCtx.getBean(MemberDao.class);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    response.setContentType("text/html;charset=UTF-8");
+    try {
+      // 클라이언트에게 번호를 요구하여 받는다.
+      int no = Integer.parseInt(request.getParameter("no"));
+      Member member = memberDao.findBy(no);
+      
+      if (member == null) {
+        throw new Exception("해당 번호의 데이터가 없습니다!");
+      }
+      request.setAttribute("member", member);
+      request.getRequestDispatcher("/jsp/member/detail.jsp").include(request, response);
+        
+    } catch (Exception e) {
+      request.setAttribute("message", e.getMessage());
+      request.setAttribute("refresh", "/member/list");
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
+    }
+  }
+
+}
