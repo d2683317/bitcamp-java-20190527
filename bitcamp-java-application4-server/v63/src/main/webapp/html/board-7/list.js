481e2a18a8957e30eb3b0ab4c9754b36b37e1216
unknown <kdjj214@nate.com>
2019-10-25 16:37:36 +0900

    Handlebars

---
+++ bitcamp-java-application4-server/v63/src/main/webapp/html/board-7/list.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var tbody = $("#list-table > tbody");
+var trTemplateSrc = $('#t1').html(); // html() ==> getter, html(param) ==> setter
+var template = Handlebars.compile(trTemplateSrc);
+loadData();
+ 
+function loadData() {
+  //console.log("서버에서 데이터 가져오기!");
+  $.get("/app/json/board/list", function(data) {
+    for (var b of data.result) {
+      $(template(b))
+        .appendTo(tbody);
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
