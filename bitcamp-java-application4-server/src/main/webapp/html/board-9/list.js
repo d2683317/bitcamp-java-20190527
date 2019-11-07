f5790886223e6e6dcb3f8764f2db0cee9fa871ee
unknown <kdjj214@nate.com>
2019-11-07 11:43:59 +0900

    목록 페이지 처리

---
+++ bitcamp-java-application4-server/src/main/webapp/html/board-9/list.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var tbody = $("#list-table > tbody");
+var trTemplateSrc = $('#t1').html(); // html() ==> getter, html(param) ==> setter
+var template = Handlebars.compile(trTemplateSrc);
+loadData();
+ 
+function loadData() {
+  //console.log("서버에서 데이터 가져오기!");
+  $.get("/app/json/board/list", function(data) {
+    tbody.html(template(data))
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
